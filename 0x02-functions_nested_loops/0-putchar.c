8d65c1d5c52d1eab8a89524ce2f535c5d4e69836
jane0001 <jantwi-boateng001@st.ug.edu.gh>
2022-12-14 16:46:01 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char *sh = "putchar";
+
+        while (*sh)
+        {
+                _putchar(*sh);
+                sh++;
+        }
+        _putchar('\n');
+
+        return (0);
+}
+
